a8d5a073f24fb994f54d44cddb60614b94a7f0f3
申普玉 <136255713@qq.com>
2023-05-22 02:00:30 +0800

    第6题

---
+++ 2023/6/env.js
@@ -0,0 +1,79 @@
+RegExp.prototype.test = function () {
+    return true
+}
+
+function createProxy(obj, targetTag) {
+    return new Proxy(obj, {
+        get(target, prop) {
+            console.log("PROXY GET: ", targetTag, prop);
+            return Reflect.get(...arguments)
+        },
+        set(target, prop, value) {
+            console.log("PROXY SET: ", targetTag, prop, " ==> ", value);
+            return Reflect.set(...arguments)
+        },
+    })
+}
+
+window = globalThis;
+let doc = {
+    addEventListener: function () {
+        console.log("document.addEventListener")
+    },
+    documentElement: createProxy({
+        style: createProxy({
+            transform: ""
+        }, "document.documentElement.style"),
+        nodeName: "HTML",
+
+    }, "document.documentElement"),
+    childNodes: [],
+    nodeType: 9,
+    defaultView: window,
+    ownerDocument: null,
+    createElement: function (e) {
+        console.log("CALL: document.createElement: ", e)
+        return createProxy({
+                nodeName: e.toUpperCase(),
+                getAttribute: function getAttribute(e) {
+                    console.log("getAttribute", e);
+                    return this[e]
+                },
+                parentNode: createProxy({
+                    removeChild: function(){
+                        console.log("document.createElement > " + e.toUpperCase() + " > parentNode" + " > removeChild")
+                    }
+                }, "document.createElement > " + e.toUpperCase() + " > parentNode")
+            }
+            , "document.createElement > " + e.toUpperCase())
+    },
+    matches: function matches() {
+        console.log("CALL: document.matches")
+    },
+    appendChild: function(e){
+        return createProxy({}, "document.appendChild > " + e.nodeName)
+    },
+    querySelectorAll:function(e){
+        return createProxy({}, "document.querySelectorAll > " + e)
+    }
+
+}
+window.document = createProxy(doc, "document")
+
+
+globalThis.deleteNodeEnv = function deleteNodeEnv() {
+    delete global;
+    delete process;
+    delete setImmediate;
+    delete Buffer;
+
+    // 虽然返回undefined，但变量还存在
+    exports = undefined;
+    module = undefined;
+    require = undefined;
+}
+
+
+exports.globalThis = globalThis
+
+
---
+++ 2023/6/test.js
@@ -0,0 +1,68 @@
+
+let myfunc = function myfunc() {
+    console.debug(arguments);
+    var aV, aW, aX, aY, aZ, b0, b1, b2, b3, b4, b5, b6, b7, b8,
+        b9, bd, be, bf;
+    aX = [];
+    aY = aX;
+    aY["length"] = 2
+    aV = arguments["length"];
+    if (aV < a0) {
+        aV = a0
+    }
+    aW = 0
+    for (; aW < aV;) {
+        aX = aY;
+        aZ = aW;
+        b0 = arguments;
+        b1 = aW;
+        b2 = b0[b1];
+        aX[aZ] = b2;
+        aW++;
+    }
+    aX = X;
+    aZ = 37129;
+    b0 = Z;
+    b1 = "concat";
+    b2 = this;
+    b3 = arguments;
+    b4 = aT;
+    b5 = [b2, b3, b4];
+    b6 = "concat";
+    b7 = aY;
+    b8 = b5["concat"](b7);
+    b9 = [b8];
+    bd = b0["concat"](b9);
+    be = a0;
+    bf = W(aX, aZ, bd, be);
+    return bf
+
+};
+let Z = [[window], [window, [1], myfunc, 1, "/api/match2023/6", {page:1}], [{url:"/api/match2023/6"}], [], myfunc, [{value:897}], "", {}];
+myfunc({value:897}, 0, {value:897})
+
+
+
+
+var W = function(Zh) {
+    var Zj, Zk, Zl, Zm, Zn, Zo, Zp, Zq, Zr, Zs, Zt, Zu, Zv, Zw, Zx, Zy, Zz;
+    Zj = sZ;
+    Zk = pQ;
+    Zl = t0;
+    Zm = Zh;
+    Zn = J0;
+    Zo = null;
+    Zp = Zh;
+    Zq = Zl + Zj + Zk;
+    Zr = J0;
+    Zs = Zp[Zq](Zr);
+    Zt = Zo !== Zs;
+    Zu = jO;
+    Zv = Zt + Zu;
+    Zw = Zh;
+    Zx = J0;
+    Zy = Zw[Zx];
+    Zz = Zv + Zy;
+    Zm[Zn] = Zz;
+    return Zh;
+};

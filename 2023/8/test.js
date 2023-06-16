ddafc0ead539bdfaacd0043f15e36f36f8b1b608
“macOS” <136255713@qq.com>
2023-06-16 18:32:17 +0800

    题目8 请求token

---
+++ 2023/8/test.js
@@ -0,0 +1,87 @@
+let a = Date.now() + ""
+let s = $.ln();
+
+A['mb'](s['h'](0, '_$a'))['cn']("update", [s['cn']('_$td', [a])])["eG"]("hex")
+A.adA(new (A.a8N(s['h'](0, '_$a'))))['cn']("update", [s['cn']('_$td', [a])])["eG"]("hex")
+
+s = new A.ir(window);
+
+new A.ir(window)['h'](0, '_$a') ===  new A.to(window._$a)
+
+
+A.adA(new (A.a8N(new A.ir(window)['h'](0, '_$a'))))['cn']("update", [new A.ir(window)['cn']('_$td', [a])])["eG"]("hex")
+
+a = "1686900182595"  // "6c7709231160b779d6b92868b534fd52"
+
+A.adg(window["_$td"].apply(window, A.hn(new A.aF([a], A.ayr(), A.af([a]).i("aF<1,@>")), !0, t.z)))
+
+
+function adi(t, n, a) {
+    try {
+        if (Object.isExtensible(t) && !Object.prototype.hasOwnProperty.call(t, n)) {
+            Object.defineProperty(t, n, {value: a})
+            return true
+        }
+    } catch (t) {
+    }
+    return !1
+}
+function adm(t, n, a) {
+    var e = t.hasOwnProperty(n) ? t[n] : null;
+    return null != e && t instanceof Object || (e = a.$1(t), A.adi(t, n, a.$1(t))), e
+}
+
+A.adA(new (A.a8N(new A.to(window._$a))))['cn']("update", [new A.ir(window)['cn']('_$td', [a])])["eG"]("hex")
+A.adA(new window["_$a"])['cn']("update", [A.adg(window["_$td"].apply(window, A.hn(new A.aF([a], A.ayr(), A.af([a]).i("aF<1,@>")), !0, t.z)))])["eG"]("hex")
+A.adm(new window["_$a"], $.aec(), new A.a9G)['cn']("update", [A.adg(window["_$td"].apply(window, A.hn(new A.aF([a], A.ayr(), A.af([a]).i("aF<1,@>")), !0, t.z)))])["eG"]("hex")
+A.adm(new window["_$a"], '___dart__$dart_dartObject_ZxYxX_0_', new A.a9G)['cn']("update", [A.adg(window["_$td"].apply(window, A.hn(new A.aF([a], A.ayr(), A.af([a]).i("aF<1,@>")), true, t.z)))])["eG"]("hex")
+
+
+(new A.a9G).$1(new window["_$a"])['cn']("update", [A.adg(window["_$td"].apply(window, A.hn(new A.aF([a], A.ayr(), A.af([a]).i("aF<1,@>")), true, t.z)))])["eG"]("hex")
+A.a9G.prototype = {$1: t => new A.ir(t), $S: 115}
+
+;(function (a) {
+    let args = [window["_$td"](...A.hn(new A.aF([a], A.ayr(), A.af([a]).i("aF<1,@>")), true, t.z))];
+    let tt = (new window["_$a"])["update"](A.hn(new A.aF(args, A.ayr(), A.af(args).i("aF<1,@>")), true, t.z)[0]);
+    return tt["hex"]()
+})(a)
+
+A = {
+    a: (t, n) => (t[v.arrayRti] = n, t),
+    aF: function (t, n, a) {
+        this.a = t, this.b = n, this.$ti = a
+    },
+    hn(t, n, a) {
+        var e, r = A.a([], a.i("z<0>"));
+        for (e = J.bE(t).gV(t); e.p();) {
+            r.push(e.gF(e));
+        }
+        return n ? r : J.Vx(r)
+    },
+    ayr: n => null == n || "string" == typeof n || "number" == typeof n || A.j6(n)
+        ? n
+        : n instanceof A.ir
+            ? n.a
+            : A.ajT(n)
+                ? n
+                : t.yn.b(n)
+                    ? n
+                    : n instanceof A.f9
+                        ? A.e2(n)
+                        : t.BO.b(n)
+                            ? A.aiO(n, "$dart_jsFunction", new A.a8O)
+                            : A.aiO(n, "_$dart_jsObject", new A.a8P($.aep()))
+}
+ab = new Proxy(A.ayr(), {
+    get(target, p, receiver) {
+        console.log("GET: ", p)
+        return Reflect.get(...arguments)
+    },
+    set(target, p, value, receiver) {
+        console.log("SET: ", p, value)
+        return Reflect.get(...arguments)
+    }
+})
+(new window["_$a"])["update"](A.hn(new A.aF([window["_$td"](a)], A.ayr(), t.zz.i("aF<1,@>")), true, t.z)[0])["hex"]();
+(new window["_$a"])["update"](A.hn(new A.aF([window["_$td"](a)], {"$1": A.a8N}), true, t.z)[0])["hex"]();
+(new window["_$a"])["update"](window["_$td"](a))["hex"]()
